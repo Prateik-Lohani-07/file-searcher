6257e2b9ddcb6499140ea7e8f228970634ad4b3f
Prateik Lohani <lohaniprateik@gmail.com>
2026-01-04 11:11:53 +0530

    updated: line no. description

--- cmd/flags.go
+++ cmd/flags.go
@@ -7,5 +7,5 @@ import (
 var (
 	Dir = flag.String("dir", "", "specifies the directory in which search operation is to be performed")
 	Query = flag.String("query", "", "specifies the keyword that is to be searched for in the search directory")
-	ShowLineNum = flag.Bool("n", false, "whether to show the line numbers where the query occurs in the file")
+	ShowLineNum = flag.Bool("n", false, "whether to show the line and column numbers where the query occurs in the file")
 )
