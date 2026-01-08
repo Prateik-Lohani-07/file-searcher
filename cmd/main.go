66eaf193373c689d1e2813df4329230e8cb086a3
Prateik Lohani <lohaniprateik@gmail.com>
2026-01-08 12:17:55 +0530

    refactor: args parsing logic into utils

--- cmd/main.go
+++ cmd/main.go
@@ -2,40 +2,17 @@ package main
 
 import (
 	"fmt"
-	"flag"
 	"log"
-	"os"
 )
 
 var Query string
 var _ = fmt.Print
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) < 2 {
-		log.Fatal("Must pass in at least 2 args (query and file pattern)")
-		os.Exit(1)
-	}
-
-	glob, paths := []string{}, []string{}
-
-	Query, glob = args[0], args[1:]
-
-	for _, g := range glob {
-		expandedGlob, err := expandGlob(g)
-
-		if err != nil {
-			log.Fatalf("Error while parsing glob pattern: %v", err)
-			os.Exit(1)
-		}
-
-		paths = append(paths, expandedGlob...)
-	}
+	var paths []string
+	Query, paths = parseArgs()
 	
 	results, err := searchAllPaths(paths)
-
 	if err != nil {
 		log.Fatalf("Failed to search for files: %v", err)
 	}
--- cmd/utils.go
+++ cmd/utils.go
@@ -2,7 +2,36 @@ package main
 
 import (
 	"path/filepath"
+	"flag"
+	"log"
+	"os"
 )
+
+func parseArgs() (string, []string) {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 {
+		log.Fatal("Must pass in at least 2 args (query and file pattern)")
+		os.Exit(1)
+	}
+
+	query, glob := args[0], args[1:]
+	paths := []string{}
+
+	for _, g := range glob {
+		expandedGlob, err := expandGlob(g)
+
+		if err != nil {
+			log.Fatalf("Error while parsing glob pattern: %v", err)
+			os.Exit(1)
+		}
+
+		paths = append(paths, expandedGlob...)
+	}
+
+	return query, paths
+}
 
 func expandGlob(glob string) ([]string, error) {
 	matches, err := filepath.Glob(glob)
